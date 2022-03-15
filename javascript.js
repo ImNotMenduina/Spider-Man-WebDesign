ead240709df49c5fb2c3eb4fa9fdf50f5c7be6d1
ImNotMenduina <mynameis_menduina@outlook.com>
2022-03-15 14:57:29 -0300

    uploading spider

---
+++ javascript.js
@@ -0,0 +1,9 @@
+document.getElementById("menu_mobile").onclick = () =>{ 
+    document.getElementById("drop").classList.toggle("revela_menu");
+    document.getElementById("mobile").classList.toggle("revela_menu");
+    
+}
+
+
+
+
